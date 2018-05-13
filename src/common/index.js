317885a0d0f6f2feaa3c337d9970e427be2d572a
Roman Mahotsky <roman_mahotsky@epam.com>
2018-05-13 17:06:27 +0300

    Add label component

--- src/common/index.js
+++ src/common/index.js
@@ -1,3 +1,4 @@
 export { Button } from './button';
 export { Input } from './input';
 export { Headline } from './headline';
+export { Label } from './label';
---
+++ src/common/label/label.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Label = (props) => {
+  return (
+    <label
+      className="label"
+      htmlFor={props.for}
+    >
+      {props.title}
+    </label>
+  )
+}
+
+export default Label;
