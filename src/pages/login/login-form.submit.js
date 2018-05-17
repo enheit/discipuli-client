aa40d60c4fbe6cc9aff4513115441b6be73faeed
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-05-17 17:06:05 +0300

    Add base64 encoding to authorization request

--- src/pages/login/login-form.submit.js
+++ src/pages/login/login-form.submit.js
@@ -1,7 +1,8 @@
 import routes from '../../routes/routes.config';
+import { Base64 } from 'js-base64';
 
 export default async ({ email, password }, { props, setErrors }) => {
-  const { data } = await props.authorize(email, password);
+  const { data } = await props.authorize(email, Base64.encode(password));
 
   if(data.Authorization) {
     const { jwtToken } = data.Authorization;
