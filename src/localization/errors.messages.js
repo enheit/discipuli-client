99c77a0868360bc989191b51c1f71eabaa671429
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-05-27 17:42:17 +0300

    Add new error messages

--- src/localization/errors.messages.js
+++ src/localization/errors.messages.js
@@ -1,4 +1,7 @@
 export default {
   required: (field) => `The ${field} is required.`,
   tooLong: (field) => `The ${field} is too long.`,
+  tooShort: (field) => `The ${field} is too short.`,
+  doNotMatch: (field) => `${field} do not match.`,
+  alreadyExist: (field) => `The ${field} already exist!`,
 }
