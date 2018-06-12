36f9146650d51c6e6ac279e2d00f87d84fe89ba7
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-06-12 22:28:22 +0300

    Change appearance of the page header depends on the user authentication

--- src/layouts/header/header.jsx
+++ src/layouts/header/header.jsx
@@ -2,28 +2,49 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { withApollo } from 'react-apollo';
 import { withRouter } from 'react-router-dom';
+import { verify } from 'jsonwebtoken';
+
 import routes from '../../routes/routes.config';
+import Authorization from '../../services/authorization.service';
 
-import { DropdownMenu } from '../../common';
+import { DropdownMenu, Button } from '../../common';
 
-const Header = (props) => {
+const LimitedHeader = (props) => {
+  return (
+    <React.Fragment>
+      <Link to={routes.login()}>Authorize me</Link> /
+      <Link to={routes.registration()}>Register me</Link>
+    </React.Fragment>
+  )
+}
 
+const ExtendedHeader = withApollo(withRouter((props) => {
   const logout = () => {
     // Reset cache os session on logout
     props.client.resetStore();
     // Remove user's token
-    localStorage.removeItem('token');
+    Authorization.clearToken();
     // Redirect user to login page
     props.history.push(routes.login());
   }
 
   return (
-    <header className="header">
+    <React.Fragment>
       <DropdownMenu title="Menu" horizontalReverse>
         <button onClick={logout} className="dropdown-menu__item">Exit</button>
       </DropdownMenu>
+    </React.Fragment>
+  )
+}));
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      { !Authorization.checkToken()
+        ? <LimitedHeader />
+        : <ExtendedHeader /> }
     </header>
   )
 }
 
-export default withApollo(withRouter(Header));
+export default Header;
