58d94e43b0d05d9fc9b7089a3d8fddd97215a900
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-06-12 22:25:06 +0300

    Change prop-types in with-filter component

--- src/render-props/with-filter.jsx
+++ src/render-props/with-filter.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class WithFilter extends Component {
-  static propTypes = {
-    render: PropTypes.func.isRequired,
-    filter: PropTypes.func.isRequired,
-    list: PropTypes.array.isRequired,
-  }
-
   render() {
     return this.props.render(this.props.list.filter(this.props.filter));
   }
+}
+
+WithFilter.propTypes = {
+  render: PropTypes.func.isRequired,
+  filter: PropTypes.func.isRequired,
+  list: PropTypes.array.isRequired,
 }
 
 export default WithFilter;
