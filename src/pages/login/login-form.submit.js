8a3074c9d9e65ab081e61b512b4f3884b8bd3054
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-06-16 13:50:05 +0300

    Change authorization service  - Adds abilities to the user after login  - Change default page after login to the /courses

--- src/pages/login/login-form.submit.js
+++ src/pages/login/login-form.submit.js
@@ -1,7 +1,11 @@
 import routes from '../../routes/routes.config';
 import { Base64 } from 'js-base64';
 
+// Services
 import Authorization from '../../services/authorization.service';
+
+// Configs
+import ability from '../../configs/ability.config';
 
 export default async ({ email, password }, { props, setErrors }) => {
   const { data } = await props.authorize(email, Base64.encode(password));
@@ -9,7 +13,14 @@ export default async ({ email, password }, { props, setErrors }) => {
   if(data.Authorization) {
     const { jwtToken } = data.Authorization;
     Authorization.setToken(jwtToken);
-    props.history.push(routes.root());
+
+    // Ger user permissions/rules
+    const rules = Authorization.getProfile().rules;
+    // Update the user permissions in the app
+    ability.update(rules);
+
+    // Redirect the user to the default page after login
+    props.history.push(routes.courses());
   } else {
     setErrors({
       email: 'The email address is invalid',
