562c41aedced96380b24678ad5eccc2d726eb4dc
Roman Mahotsky <roman.mahotskyi@gmail.com>
2018-06-16 13:04:50 +0300

    Adds Ability service

---
+++ src/configs/ability.config.js
@@ -0,0 +1,11 @@
+// Services
+import Ability from '../services/ability.service';
+import Authorization from '../services/authorization.service';
+
+const ability = new Ability();
+
+if(Authorization.getProfile()) {
+  ability.update(Authorization.getProfile().rules);
+}
+
+export default ability;
---
+++ src/services/ability.service.js
@@ -0,0 +1,61 @@
+export class Rule {
+  constructor(actions, subject) {
+    this.actions = actions;
+    this.subject = subject;
+  }
+
+  add(action) {
+    this.actions = [...this.actions, action];
+  }
+
+  has(action) {
+    return this.actions.some(existingAction => existingAction === action);
+  }
+}
+
+class Ability {
+  constructor(abilities = []) {
+    this.abilities = abilities;
+  }
+
+  update(abilities) {
+    if(!Array.isArray(abilities)) {
+      throw new Error('Expected an array');
+    }
+
+    this.abilities = abilities.map(rule => {
+      return new Rule(rule.actions, rule.subject);
+    });
+  }
+
+  extend(ability) {
+    this.abilities = [...this.abilities, ability];
+  }
+
+  getRule(subject) {
+    return this.abilities.find(rule => rule.subject === subject);
+  }
+
+  exist(action, subject) {
+    const rule = this.getRule(subject);
+
+    return !!rule && rule.has(action);
+  }
+
+  can(action, subject) {
+    const rule = this.getRule(subject);
+    if (!!rule) {
+      if (!rule.has(action)) {
+        rule.add(action);
+      }
+    } else {
+      this.extend(new Rule([action], subject));
+    }
+  }
+
+  define(callback) {
+    callback(this.can.bind(this));
+  }
+}
+
+export default Ability;
